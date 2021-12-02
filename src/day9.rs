fcfacb84f020ef55edea773547b294c8c9259a1d
Bryan Bates <bryancbates@gmail.com>
2021-12-02 09:22:23 -0600

    templating out the remaining days

---
+++ src/day9.rs
@@ -0,0 +1,39 @@
+type Chunk = Vec<String>;
+
+// Default implementation: Chunk on whitespace
+#[aoc_generator(day9)]
+pub fn input_generator(input: &str) -> Vec<Vec<String>> {
+    input.lines().map(|l| {
+        l.trim().split(' ').map(|s| s.to_owned()).collect::<Chunk>()
+    }).collect()
+}
+
+
+#[aoc(day9, part1)]
+pub fn part1(input: &[Chunk]) -> u32 {
+    input.len() as u32
+}
+
+#[aoc(day9, part2)]
+pub fn part2(input: &[Chunk]) -> u32 {
+    input.iter().map(|c| c.len()).sum::<usize>() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = r#"a b c
+d e f
+"#;
+
+    #[test]
+    fn part1_ex1() {
+        assert_eq!(part1(&input_generator(EXAMPLE_INPUT)), 2)
+    }
+
+    #[test]
+    fn part2_ex1() {
+        assert_eq!(part2(&input_generator(EXAMPLE_INPUT)), 6)
+    }
+}
