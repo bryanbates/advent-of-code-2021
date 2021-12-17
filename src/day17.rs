67e514372cae3c67915b842e2d73d8574b3a74e9
Bryan Bates <bryancbates@gmail.com>
2021-12-17 06:50:22 -0600

    day 17 cleanup

--- src/day17.rs
+++ src/day17.rs
@@ -52,18 +52,12 @@ fn parse_input(input: &str) -> Target {
 
 fn step((x, y): Position, (dx, dy): Velocity) -> (Position, Velocity) {
     let pos = (x + dx, y + dy);
-    let ddx = if dx > 0 {
-        -1
-    } else if dx == 0 {
-        0
-    } else {
-        1
-    };
+    let ddx = if dx > 0 { -1 } else { 0 };
     let vel = (dx + ddx, dy - 1);
     (pos, vel)
 }
 
-fn iterate(v0: Velocity, t: Target) -> i32 {
+fn iterate(v0: Velocity, t: Target) -> Option<i32> {
     let mut p: Position = (0, 0);
     let mut v: Velocity = v0;
 
@@ -74,35 +68,37 @@ fn iterate(v0: Velocity, t: Target) -> i32 {
     let max_x = t.0.clone().max().unwrap();
     let min_y = t.1.clone().min().unwrap();
 
+    // Are we past our target zone?
     while p.0 <= max_x && p.1 >= min_y {
         let res = step(p, v);
         p = res.0;
         v = res.1;
         max_y = max(max_y, p.1);
-        if t.0.contains(&p.0) && t.1.clone().contains(&p.1) {
+        if t.0.contains(&p.0) && t.1.contains(&p.1) {
             on_target = true;
-            println!("In target! {:?}", v0);
+            // println!("In target! {:?}", v0);
         }
     }
 
     if on_target {
-        max_y
+        Some(max_y)
     } else {
-        -1000
+        None
     }
 }
 
 #[aoc(day17, part1)]
 pub fn part1(input: &str) -> i32 {
     let (xrange, yrange) = parse_input(input);
-    println!("{:?}, {:?}", xrange, yrange);
+    // println!("{:?}, {:?}", xrange, yrange);
 
-    let mut max_y = -1000;
+    let mut max_y = -100;
 
-    for dx in 0..1000 {
-        for dy in 0..1000 {
-            let dv = iterate((dx, dy), (xrange.clone(), yrange.clone()));
-            max_y = max(dv, max_y);
+    for dx in 0..=xrange.clone().max().unwrap() {
+        for dy in -100..100 {
+            if let Some(dv) = iterate((dx, dy), (xrange.clone(), yrange.clone())) {
+                max_y = max(max_y, dv);
+            }
         }
     }
 
@@ -112,14 +108,13 @@ pub fn part1(input: &str) -> i32 {
 #[aoc(day17, part2)]
 pub fn part2(input: &str) -> usize {
     let (xrange, yrange) = parse_input(input);
-    println!("{:?}, {:?}", xrange, yrange);
+    // println!("{:?}, {:?}", xrange, yrange);
 
     let mut ds: HashSet<Velocity> = HashSet::new();
 
-    for dx in 0..1000 {
-        for dy in -500..1000 {
-            let dv = iterate((dx, dy), (xrange.clone(), yrange.clone()));
-            if dv != -1000 {
+    for dx in 0..=xrange.clone().max().unwrap() {
+        for dy in -100..100 {
+            if iterate((dx, dy), (xrange.clone(), yrange.clone())).is_some() {
                 ds.insert((dx, dy));
             }
         }
